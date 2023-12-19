729b20ccb2e1819d5255784a9e3deedaaa78990f
dmcheong <dcheong@DESKTOP-CH25SRM>
2023-12-19 19:12:54 +0100

    add ssl connect for MongoDB Atlas

--- src/config/mongodb-config.js
+++ src/config/mongodb-config.js
@@ -1,17 +1,24 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+
+const caPath = path.resolve(__dirname,  process.env.CA_PATH || './ca.pem');
 
 const uri = process.env.MONGODB_URI;
 
-// // Nouvelle configuration de connexion MongoDB
-// const mongoOptions = {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// };
+// Nouvelle configuration de connexion MongoDB
+const mongoOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  // Ajoutez ces options pour activer la connexion SSL/TLS
+  ssl: true,
+  sslValidate: true,
+  sslCA: Buffer.from(fs.readFileSync(caPath)), // Remplacez par le chemin vers votre certificat CA
+};
 
 // Connexion à la base de données MongoDB
-// mongoose.connect(uri, mongoOptions);
-mongoose.connect(uri);
+mongoose.connect(uri, mongoOptions);
 
 const connection = mongoose.connection;
 connection.once('open', () => {
