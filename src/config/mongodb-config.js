442ebe18c00f18952e226d1e89e4d9ab5994e3c4
dmcheong <dcheong@DESKTOP-CH25SRM>
2023-12-19 20:41:43 +0100

    Mongodb tls repair

--- src/config/mongodb-config.js
+++ src/config/mongodb-config.js
@@ -1,23 +1,18 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const fs = require('fs');
 const path = require('path');
 
-const caPath = path.resolve(__dirname,  process.env.CA_PATH || './ca.pem');
-
+const caPath = path.resolve(__dirname, './ca.pem');
 const uri = process.env.MONGODB_URI;
 
-// Nouvelle configuration de connexion MongoDB
 const mongoOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  // Ajoutez ces options pour activer la connexion SSL/TLS
-  ssl: true,
-  sslValidate: true,
-  sslCA: Buffer.from(fs.readFileSync(caPath)), // Remplacez par le chemin vers votre certificat CA
+  tls: true,
+  tlsCAFile: caPath,
+  tlsAllowInvalidCertificates: true,
 };
 
-// Connexion à la base de données MongoDB
 mongoose.connect(uri, mongoOptions);
 
 const connection = mongoose.connection;
@@ -33,7 +28,6 @@ connection.on('disconnected', () => {
   console.log('Déconnexion de MongoDB');
 });
 
-// Fermer la connexion MongoDB lors de l'arrêt de l'application
 process.on('SIGINT', () => {
   connection.close(() => {
     console.log('Fermeture de la connexion MongoDB à l\'arrêt de l\'application');
